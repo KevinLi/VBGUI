0623e61cf866ce77ccff4b50661ae03f189d69a7
Kevin Li <nerd_rage@live.com>
2010-11-10 21:42:03 -0500

    first commit

---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Dim RandomClass As New Random()
+
+    Dim RandomNumber0 As Integer, RandomNumber1 As Integer, RandomNumber2 As Integer, RandomNumber3 As Integer
+
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
+        RandomNumber0 = RandomClass.Next(0, 255)
+        RandomNumber1 = RandomClass.Next(0, 255)
+        RandomNumber2 = RandomClass.Next(0, 255)
+        RandomNumber3 = RandomClass.Next(0, 255)
+        Label1.Text = RandomNumber0.ToString + "." + RandomNumber1.ToString + "." + RandomNumber2.ToString + "." + RandomNumber3.ToString
+    End Sub
+End Class
